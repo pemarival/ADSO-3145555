a2943105fb0c13578101aa52fabfdcf4d4a7ed65
pemarival <mvalenzuelapenagos@gmail.com>
2026-04-29 17:03:12 -0500

    add link to week 2 in date 18-02-26

--- 03-execution/02-week/03-optional-activity/18-02-26/test/src/main/java/com/sena/test/IRepository/IUserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.sena.test.IRepository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.sena.test.Entity.User;
-
-public interface IUserRepository extends JpaRepository <User, Long> {
-
-}
